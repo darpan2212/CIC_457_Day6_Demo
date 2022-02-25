8a97e5168be1f9660540057dfef24ecdf9b595c2
Darpan Madlani <Darpan@proclink.com>
2022-02-25 12:23:31 +0530

    for loop demo programs are done

---
+++ calculateMonthlyWage.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PRESENT=1;
+PART_TIME=2;
+WAGE_PER_HOUR=20;
+MAX_WORKING_DAY=20;
+
+totalWage=0;
+
+for ((day=0;day<$MAX_WORKING_DAY;day++))
+do
+	isPresent=$((RANDOM%3));
+
+	case $isPresent in
+		$PRESENT)
+			empWage=$(($WAGE_PER_HOUR * 8));
+		;;
+
+		$PART_TIME)
+			empWage=$(($WAGE_PER_HOUR * 4));
+		;;
+
+		*)
+			empWage=0;
+		;;
+	esac;
+	totalWage=$((totalWage+empWage));
+done
+
+echo "Employee Wage : $"$totalWage "USD";
---
+++ forloop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for ((cnt=0;cnt<10;cnt++))
+do
+	echo -n -e $cnt"\n";
+done
